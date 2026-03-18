cf888f0c0f1234a8c079712534247462c224b0ec
FajeSu <fajesu@ya.ru>
2026-03-18 12:15:40 +0500

    Added able to set API version by environment variable

--- vkpymusic/vk_api/vk_api_params.py
+++ vkpymusic/vk_api/vk_api_params.py
@@ -1,10 +1,11 @@
 # Default parameters for requests
+import os
 from typing import Dict, Union
 
 
 BASE_URL: str = "https://api.vk.com/method/"
 
-DEFAULT_VERSION: str = "5.131"
+DEFAULT_VERSION: str = os.getenv("VKPYMUSIC_API_VERSION", "5.131")
 
 DEFAULT_LANG: str = "ru"
 
